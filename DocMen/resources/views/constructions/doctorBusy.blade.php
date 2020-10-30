e8fbe707513f3a0f3b2c87dcdbe461d95e8827fc
Asadullah <md.asadullah196@gmail.com>
2020-10-30 12:30:46 +0600

    DocMen Initilization has done in this stage. Basic view design for dr busy message. Debug little bit for adding external resouces.

---
+++ DocMen/resources/views/constructions/doctorBusy.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <title>Doctore In Leave</title>
+<body>
+
+    <div class="leave" bgcolor="#000">
+        <div class="leave-bg">
+        
+        </div>
+        <div class="leave-body">
+            <div class="leave-body-left">
+                <img src="{{ asset('images/galib1.png') }}" alt="Galib" width="" height="">
+            </div>
+            <div class="leave-body-right">
+                <img src="{{ asset('images/DocMenLogo.png') }}" alt="DocMen Logo" width="" height="">
+                <h3>Dr. Is In Leave</h3>
+				<p>Dr. Galib is in leave now! He is doing 3 month advance training on pathology! He will be available in next year july. For more details, you can contuct us!</p>
+				<p>For advance appointment email at <a href="">appointment@drgalib.com</a></p>
+				
+				<div class="social-link">
+					<a href="#" class="fa fa-facebook"></a>
+					<a href="#" class="fa fa-twitter"></a>
+					<a href="#" class="fa fa-facebook"></a>
+					<a href="#" class="fa fa-twitter"></a>
+					<a href="#" class="fa fa-facebook"></a>
+				</div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
