487d24d1e3d8cae66148473eda48935f748460a5
Asadullah <md.asadullah196@gmail.com>
2020-10-30 17:14:51 +0600

    Underconstcution veiw has designed. DocMen First view complated for visitor part!

--- DocMen/resources/views/constructions/doctorBusy.blade.php
+++ DocMen/resources/views/constructions/doctorBusy.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
     <title>Doctore In Leave</title>
+	<link rel="stylesheet" href="{{ asset('/css/main.css') }}" type="text/css">
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body>
 
-    <div class="leave" bgcolor="#000">
+    <div class="leave">
         <div class="leave-bg">
         
         </div>
@@ -13,17 +16,21 @@
             </div>
             <div class="leave-body-right">
                 <img src="{{ asset('images/DocMenLogo.png') }}" alt="DocMen Logo" width="" height="">
-                <h3>Dr. Is In Leave</h3>
+                <h2>Dr Galib is in leave for advance training!</h2>
 				<p>Dr. Galib is in leave now! He is doing 3 month advance training on pathology! He will be available in next year july. For more details, you can contuct us!</p>
-				<p>For advance appointment email at <a href="">appointment@drgalib.com</a></p>
 				
-				<div class="social-link">
-					<a href="#" class="fa fa-facebook"></a>
-					<a href="#" class="fa fa-twitter"></a>
-					<a href="#" class="fa fa-facebook"></a>
-					<a href="#" class="fa fa-twitter"></a>
-					<a href="#" class="fa fa-facebook"></a>
-				</div>
+				<p id="contact-email">For advance appointment email at <b><a href="#">appointment@drgalib.com</a></b></p>
+				
+				<ul>
+					<li><a href="#" class="fa fa-facebook-square" style="font-size:50px;color:#00aeef"></a></li>
+					<li><a href="#" class="fa fa-twitter-square" style="font-size:50px;color:#00aeef"></a></li>
+					<li><a href="#" class="fa fa-instagram" style="font-size:50px;color:#00aeef"></a></li>
+					<li><a href="#" class="fa fa-linkedin-square" style="font-size:50px;color:#00aeef"></a></li>
+					<li><a href="#" class="fa fa-quora" style="font-size:50px;color:#00aeef"></a></li>
+				</ul>
+				
+				<h4>copyright &copy; 2020 all rights reserved by <a href="#">DocMen</a>&nbsp;</h4>
+				
             </div>
         </div>
     </div>
