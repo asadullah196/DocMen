3801fcf793b6d22b86827cb8b065ae65c6567f0e
Asadullah <md.asadullah196@gmail.com>
2020-11-21 14:45:20 +0600

    Vue Js installed successfully inside laravel

--- DocMen/resources/js/app.js
+++ DocMen/resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+Vue.component('example-component',require('./components/ExampleComponents.vue'));
+const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
+
+
+  const app = new Vue({
+    router
+  }).$mount('#app')
--- DocMen/resources/views/docIndex.blade.php
+++ DocMen/resources/views/docIndex.blade.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html>
     <title>DocMen - One stop solutions for doctors chamber management</title>
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 <body>
 
-    <center>
-        <h1>Hello Doctors! You are our HERO so DocMen is completly free for you!</h1>
-        <p>SignUp Today</p>
-    </center>
+    <div class="dibody">
+        <center>
+            <h1>Hello Doctors! You are our HERO so DocMen is completly free for you!</h1>
+            <p>SignUp Today</p>
+        </center>
+    </div>
+
+    <div id="app">
+        <h1>Hello App!</h1>
+        <p>
+            <!-- use router-link component for navigation. -->
+            <!-- specify the link by passing the `to` prop. -->
+            <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+            <router-link to="/foo">Go to Foo</router-link>
+            <router-link to="/bar">Go to Bar</router-link>
+        </p>
+        <!-- route outlet -->
+        <!-- component matched by the route will render here -->
+        <router-view></router-view>
+</div>
 
 </body>
 </html>
